c1e6f8c24b3e7b330db959b741f2595430dfd7b6
Kevin Cui <bh@bugs.cc>
2024-09-12 14:32:07 +0800

    refactor(scripts): improve download ovm resource
    Signed-off-by: Kevin Cui <bh@bugs.cc>

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,100 +1,101 @@
 import fs from "node:fs";
 import fsP from "node:fs/promises";
 import got from "got";
-import { join, dirname } from "node:path";
+import { join, dirname, basename } from "node:path";
 import { pipeline } from "node:stream/promises";
 import { fileURLToPath } from "node:url";
 import { homedir } from "node:os";
+import crypto from "node:crypto";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+if (!["darwin", "win32"].includes(process.platform) || (process.platform === "win32" && process.arch !== "x64")) {
+    console.warn("[OVM]: Currently only supports MacOS(arm64 and x64) and Windows(x64)");
+    process.exit(0);
+}
+
 const packageJSON = JSON.parse(fs.readFileSync(join(__dirname, "..", "package.json"), "utf-8"));
-const coreVersion = packageJSON.ovm[process.platform]?.core;
-const ovmVersion = packageJSON.ovm[process.platform]?.ovm;
 
-const binName = process.platform === "win32" ? "ovm.exe" : "ovm";
+const tasks = [
+    ...toTasks(packageJSON.ovm[`${process.platform}-core`], "core"),
+    ...toTasks(packageJSON.ovm[`${process.platform}-ovm`], "ovm"),
+];
 
-const assets = {
-    x64: {
-        core: [],
-        ovm: "",
-    },
-    arm64: {
-        core: [],
-        ovm: "",
-    },
-}
-
-switch (process.platform) {
-    case "darwin": {
-        const coreURL = `http://github.com/oomol-lab/ovm-core/releases/download/${coreVersion}`;
-        const ovmURL = `http://github.com/oomol-lab/ovm/releases/download/${ovmVersion}`;
-
-        assets.x64.core.push(
-            `${coreURL}/applehv-rootfs-amd64.rootfs.erofs#rootfs.erofs`,
-            `${coreURL}/initrd-amd64.initrd.gz#initrd.gz`,
-            `${coreURL}/kernel-amd64.bzImage#bzImage`
-        );
-        assets.x64.ovm = `${ovmURL}/ovm-amd64#${binName}`;
-
-        assets.arm64.core.push(
-            `${coreURL}/applehv-rootfs-arm64.rootfs.erofs#rootfs.erofs`,
-            `${coreURL}/initrd-arm64.initrd.gz#initrd.gz`,
-            `${coreURL}/kernel-arm64.Image#Image`,
-        );
-        assets.arm64.ovm = `${ovmURL}/ovm-arm64#${binName}`;
-
-        break;
-    }
-    case "win32": {
-        const coreURL = `http://github.com/oomol-lab/ovm-builder/releases/download/${coreVersion}`;
-        const ovmURL = `http://github.com/oomol/ovm-win/releases/download/${ovmVersion}`;
-
-        assets.x64.core.push(`${coreURL}/rootfs-amd64.zst#rootfs.zst`);
-        assets.x64.ovm = `${ovmURL}/ovm-amd64.exe#${binName}`;
-
-        if (process.arch !== "x64") {
-            console.warn("[OVM]: Windows only supports x64 architecture");
-            process.exit(0);
-        }
-
-        break;
-    }
-    default: {
-        console.warn("[OVM]: Currently only supports macOS and Windows");
-        process.exit(0);
-    }
-}
-
-const cacheDir = join(homedir(), ".cache", "ovm", packageJSON.version);
+const cacheDir = join(homedir(), ".cache", "ovm");
 await fsP.mkdir(cacheDir, { recursive: true });
 
 const vmResourcesPath = join(__dirname, "..", "vm-resources");
 await fsP.mkdir(vmResourcesPath, { recursive: true });
 
-const tasks = [...assets[process.arch].core, assets[process.arch].ovm];
+for (const { url, outputName, sha256, version, type } of tasks) {
+    const cache = join(cacheDir, `${type}-${version}`, outputName);
+    await fsP.mkdir(dirname(cache), { recursive: true });
 
-for (const t of tasks) {
-    const [url, filename] = t.split("#");
-    const outputPathWithCache = join(cacheDir, filename);
-    const outputPath = join(vmResourcesPath, filename);
+    await download(5)(url, cache, sha256);
 
-    if (fs.existsSync(outputPathWithCache)) {
-        console.log(`[OVM]: Found ${filename} in cache`);
-        await fsP.copyFile(outputPathWithCache, outputPath);
-        continue;
-    }
-
-    console.log("[OVM]: Downloading", url, "to", outputPath);
-    await pipeline(got.stream(url), fs.createWriteStream(outputPathWithCache))
-    await fsP.copyFile(outputPathWithCache, outputPath);
+    const outputPath = join(vmResourcesPath, outputName);
+    await fsP.copyFile(cache, outputPath);
     console.log(`[OVM]: Downloaded ${outputPath}`);
 }
 
+const binName = process.platform === "win32" ? "ovm.exe" : "ovm";
 const ovmStat = await fsP.stat(join(vmResourcesPath, binName));
 if (!(ovmStat.mode & fs.constants.X_OK)) {
     await fsP.chmod(join(vmResourcesPath, binName), 0o755);
 }
 
 console.log("[OVM]: Downloaded successfully");
+
+function toTasks (metadata, type) {
+    return metadata.info[process.arch].map(f => {
+        const [remoteFile, outputName, sha256] = f.split("#");
+        return {
+            url: `${metadata.endpoint}/${metadata.version}/${remoteFile}`,
+            outputName,
+            sha256,
+            version: metadata.version,
+            type,
+        };
+    })
+}
+
+function download(retry) {
+    let r = retry;
+    return async function doDownload(url, output, sha256, isRetry = false) {
+        if (r === 0) {
+            console.error("[OVM]: Failed to download the file");
+            process.exit(1);
+        }
+        r--;
+
+        if (fs.existsSync(output)) {
+            const cacheHash = await computeHash(output);
+            if (cacheHash === sha256) {
+                isRetry || console.info(`[OVM]: Find ${basename(output)} in cache, hash matched`);
+                return;
+            }
+
+            console.warn(`[OVM]: Hash mismatch for ${basename(output)}, Expected: ${sha256}, Actual: ${cacheHash}`);
+            await fsP.rm(output, { force: true });
+        }
+
+        console.log("[OVM]: Downloading", url, "to", output);
+        try {
+            await pipeline(got.stream(url), fs.createWriteStream(output));
+        } catch (_err) {
+            console.warn("[OVM]: Download failed, retrying...");
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            await fsP.rm(output, { force: true });
+        }
+
+        await doDownload(url, output, sha256, true);
+    }
+}
+
+async function computeHash(filePath) {
+    const input = fs.createReadStream(filePath);
+    const hash = crypto.createHash("sha256");
+    await pipeline(input, hash);
+    return hash.digest("hex");
+}
