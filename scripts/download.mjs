bda3a0801995e163e82aa8cab81a2b886c2f5da9
Kevin Cui <bh@bugs.cc>
2024-10-23 17:48:09 +0800

    fix(script): cannot download ovm resources in postinstall
    Signed-off-by: Kevin Cui <bh@bugs.cc>

---
+++ scripts/download.mjs
@@ -0,0 +1,112 @@
+import fs from "node:fs";
+import fsP from "node:fs/promises";
+import { join, dirname, basename } from "node:path";
+import { pipeline } from "node:stream/promises";
+import { fileURLToPath } from "node:url";
+import { homedir } from "node:os";
+import crypto from "node:crypto";
+import { tgz } from "compressing";
+import { Readable } from "node:stream";
+import { finished } from "node:stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const resourceJSON = JSON.parse(fs.readFileSync(join(__dirname, "resource.json"), "utf-8"));
+
+const info = resourceJSON[`${process.platform}-${process.arch}`];
+if (!info) {
+    const supportList = Object.keys(resourceJSON).map(item => item.split("-").join(" ")).join(", ");
+    console.warn(`[OVM]: Currently only supports ${supportList}`);
+    process.exit(0);
+}
+
+const cacheDir = join(homedir(), ".cache", "ovm");
+const distDir = join(__dirname, "vm-resources");
+await fsP.mkdir(distDir, { recursive: true });
+
+for (const { name, version, download: downloadTemplate, sha256, out } of info) {
+    const downloadURL = downloadTemplate.replace(/\{version}/g, version);
+    const cachePath = join(cacheDir, `${name}-${version}`, ...out.split("/"));
+    await fsP.mkdir(dirname(cachePath), { recursive: true });
+
+    await download(5)(downloadURL, cachePath, sha256);
+
+    let distPath;
+    if (out.endsWith(".tar.gz")) {
+        distPath = join(distDir, out.replace(".tar.gz", ""));
+        await fsP.rm(distPath, { force: true, recursive: true });
+        await tgz.uncompress(cachePath, distPath)
+    } else {
+        distPath = join(distDir, out);
+        await fsP.rm(distPath, { force: true, recursive: true });
+        await fsP.mkdir(dirname(distPath), { recursive: true });
+        await fsP.copyFile(cachePath, distPath);
+    }
+
+    console.log(`[OVM]: Downloaded ${distPath}`);
+}
+
+const files = await fsP.readdir(distDir, { withFileTypes: true, recursive: true });
+for (const file of files) {
+    if (file.isDirectory()) {
+        continue;
+    }
+    if (["gvproxy", "krunkit", "ovm", "ovm.exe"].includes(file.name)) {
+        await fsP.chmod(join(file.path, file.name), 0o755);
+    }
+}
+
+console.log("[OVM]: Downloaded successfully");
+
+function download(retry) {
+    let r = retry;
+    return async function doDownload(url, output, sha256, isRetry = false) {
+        if (r === 0) {
+            console.error("[OVM]: Failed to download the file");
+            process.exit(1);
+        }
+        r--;
+
+        if (fs.existsSync(output)) {
+            const cacheHash = await computeHash(output);
+            if (cacheHash === sha256) {
+                isRetry || console.info(`[OVM]: Find ${basename(output)} in cache, hash matched`);
+                return;
+            }
+
+            console.warn(`[OVM]: Hash mismatch for ${basename(output)}, Expected: ${sha256}, Actual: ${cacheHash}`);
+            await fsP.rm(output, { force: true });
+        }
+
+        console.log("[OVM]: Downloading", url, "to", output);
+        try {
+            await downloadSteam(url, output)
+        } catch (_err) {
+            console.warn("[OVM]: Download failed, retrying...");
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            await fsP.rm(output, { force: true });
+        }
+
+        await doDownload(url, output, sha256, true);
+    }
+}
+
+async function computeHash(filePath) {
+    const input = fs.createReadStream(filePath);
+    const hash = crypto.createHash("sha256");
+    await pipeline(input, hash);
+    return hash.digest("hex");
+}
+
+
+async function downloadSteam (url, dest) {
+    const resp = await fetch(url);
+    if (!resp.ok || !resp.body) {
+        throw new Error(`unexpected response ${resp.statusText}`);
+    }
+
+    const file = fs.createWriteStream(dest);
+    await finished(Readable.fromWeb(resp.body)
+        .pipe(file));
+}
--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,99 +1,45 @@
-import fs from "node:fs";
 import fsP from "node:fs/promises";
-import got from "got";
-import { join, dirname, basename } from "node:path";
-import { pipeline } from "node:stream/promises";
+import os from "node:os";
+import path, { dirname } from "node:path";
+import cp from "node:child_process";
 import { fileURLToPath } from "node:url";
-import { homedir } from "node:os";
-import crypto from "node:crypto";
-import { tgz } from "compressing";
+import fs, { constants } from "node:fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const resourceJSON = JSON.parse(fs.readFileSync(join(__dirname, "..", "resource.json"), "utf-8"));
+const workspace = await fsP.mkdtemp(path.join(os.tmpdir(), "ovm-download-"));
 
-const info = resourceJSON[`${process.platform}-${process.arch}`];
-if (!info) {
-    const supportList = Object.keys(resourceJSON).map(item => item.split("-").join(" ")).join(", ");
-    console.warn(`[OVM]: Currently only supports ${supportList}`);
-    process.exit(0);
-}
+await Promise.all([
+    fsP.cp(path.join(__dirname, "download.mjs"), path.join(workspace, "download.mjs"), { force: true }),
+    fsP.cp(path.join(__dirname, "resource.json"), path.join(workspace, "resource.json"), { force: true }),
+    fsP.cp(path.join(__dirname, "package.json"), path.join(workspace, "package.json"), { force: true }),
+    fsP.cp(path.join(__dirname, "package-lock.json"), path.join(workspace, "package-lock.json"), { force: true }),
+])
 
-const cacheDir = join(homedir(), ".cache", "ovm");
-const distDir = join(__dirname, "..", "vm-resources");
-await fsP.mkdir(distDir, { recursive: true });
+cp.execSync("npm install", {
+    cwd: workspace,
+    encoding: "utf-8",
+    shell: true,
+    stdio: "inherit",
+});
 
-for (const { name, version, download: downloadTemplate, sha256, out } of info) {
-    const downloadURL = downloadTemplate.replace(/\{version}/g, version);
-    const cachePath = join(cacheDir, `${name}-${version}`, ...out.split("/"));
-    await fsP.mkdir(dirname(cachePath), { recursive: true });
+cp.execSync("node download.mjs", {
+    cwd: workspace,
+    encoding: "utf-8",
+    shell: true,
+    stdio: "inherit",
+});
 
-    await download(5)(downloadURL, cachePath, sha256);
-
-    let distPath;
-    if (out.endsWith(".tar.gz")) {
-        distPath = join(distDir, out.replace(".tar.gz", ""));
-        await fsP.rm(distPath, { force: true, recursive: true });
-        await tgz.uncompress(cachePath, distPath)
-    } else {
-        distPath = join(distDir, out);
-        await fsP.rm(distPath, { force: true, recursive: true });
-        await fsP.mkdir(dirname(distPath), { recursive: true });
-        await fsP.copyFile(cachePath, distPath);
+fs.access(path.join(workspace, "vm-resources"), constants.F_OK, async (err) => {
+    if (!err) {
+        console.log(`[OVM]: Move ${path.join(workspace, "vm-resources")} to ${path.join(__dirname, "..", "vm-resources")}`);
+        await fsP.cp(path.join(workspace, "vm-resources"), path.join(__dirname, "..", "vm-resources"), {
+            recursive: true,
+            force: true,
+            preserveTimestamps: true,
+        });
     }
 
-    console.log(`[OVM]: Downloaded ${distPath}`);
-}
-
-const files = await fsP.readdir(distDir, { withFileTypes: true, recursive: true });
-for (const file of files) {
-    if (file.isDirectory()) {
-        continue;
-    }
-    if (["gvproxy", "krunkit", "ovm", "ovm.exe"].includes(file.name)) {
-        await fsP.chmod(join(file.path, file.name), 0o755);
-    }
-}
-
-console.log("[OVM]: Downloaded successfully");
-
-function download(retry) {
-    let r = retry;
-    return async function doDownload(url, output, sha256, isRetry = false) {
-        if (r === 0) {
-            console.error("[OVM]: Failed to download the file");
-            process.exit(1);
-        }
-        r--;
-
-        if (fs.existsSync(output)) {
-            const cacheHash = await computeHash(output);
-            if (cacheHash === sha256) {
-                isRetry || console.info(`[OVM]: Find ${basename(output)} in cache, hash matched`);
-                return;
-            }
-
-            console.warn(`[OVM]: Hash mismatch for ${basename(output)}, Expected: ${sha256}, Actual: ${cacheHash}`);
-            await fsP.rm(output, { force: true });
-        }
-
-        console.log("[OVM]: Downloading", url, "to", output);
-        try {
-            await pipeline(got.stream(url), fs.createWriteStream(output));
-        } catch (_err) {
-            console.warn("[OVM]: Download failed, retrying...");
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            await fsP.rm(output, { force: true });
-        }
-
-        await doDownload(url, output, sha256, true);
-    }
-}
-
-async function computeHash(filePath) {
-    const input = fs.createReadStream(filePath);
-    const hash = crypto.createHash("sha256");
-    await pipeline(input, hash);
-    return hash.digest("hex");
-}
+    await fsP.rm(workspace, { recursive: true, force: true });
+});
